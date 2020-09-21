9546c300235f7986d6d1cbd53bc173710b312002
jklausz <jklausz1281@gmail.com>
2020-09-21 16:41:41 +0200

    usershbs usersjs

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,40 @@ router.get("/", async function(req, res, next) {
     res.render("users", { title: "Adatok", userData });
 });
 
+/*router.get('/users', async function(req, res, next) => {
+    const userMix = await knex("users").select();
+    res.render("users", {
+        name: "",
+        email: "",
+        address: ""
+    });
+}); */
+
+/*router.post("/", (req, res, next) => {
+    // res.json(req.body).send();
+    const userDTO = req.body;
+
+    if (!isUserFormValid(userDTO)) {
+        res.render('error', {
+            message: "Hibás form kitöltés!"
+        });
+    } else {
+        res.send(isUserFormValid(userDTO.name));
+    }
+}
+});
+
+const isUserFormValid = (users) => {
+    return users.name && users.email && users.address;
+};
+
+const isUserExist = async(name) => {
+    const count = await knex("users")
+        .count("name")
+        .where("name", name);
+    return count > 0;
+
+}; */
+
+
 module.exports = router;
